2e1c953f31dcd66f73fb67613cabc99e3944e571
MaxO <maxi.opel.16@gmail.com>
2025-04-21 20:58:11 +0200

    adding a help and description

--- Functions/__init__.py
+++ Functions/__init__.py
@@ -1,8 +1,11 @@
 from math import sqrt, factorial
 from prettytable import PrettyTable
 from sympy import symbols, Eq, solve
+import keyboard
 
 from Functions.Binominal_Distribution import *
 from Functions.Primes import *
+from Functions.Zufallsgroeße import *
+from Functions.help import *
 
 
--- main.py
+++ main.py
@@ -3,12 +3,20 @@ from Functions.Fibonacci import fibonacci
 from Functions.Zufallsgroeße import tabelle_random_var
 
 if __name__ == '__main__':
+    print("┌─────┬───┐ Welcome ShellGebra. \n"
+          "│     │   │ Your Personal Math Assistant in your shell.\n"
+          "│     ├─┬─┤ Type help to find a menu with introductions.\n"
+          "└─────┴─┴─┘")
+
     while True:
         user_input = input(">> ")
 
 
         if user_input.lower() == "exit":
             exit()
+
+        elif user_input.lower() == "help":
+            help_and_descriptions()
 
 
         elif user_input.startswith("do "):
@@ -35,6 +43,7 @@ if __name__ == '__main__':
             except ValueError:
                 print("Your input was not a valid integer")
 
+
         elif user_input.startswith("fib "):
             try:
                 print(fibonacci(int(user_input[4:])))
@@ -52,14 +61,14 @@ if __name__ == '__main__':
                 print("Your input was not a valid integer")
 
 
-        #TODO this needs to be fixed there is a Value Error even with good input
         elif user_input == "random var":
             pass
             name = input ("Name of the Experiment: ")
             xi = input("The values of xi: ")
             Pxi = input("The probabilities of the xi`s: ")
             try:
-                tabelle_random_var(name, [float(x.strip()) for x in xi.split(",")], [float(x.strip()) for x in Pxi.split(",")])
+        # TODO this needs to be fixed there is a problem with the input of lists
+                tabelle_random_var(name, xi, Pxi)
             except ValueError:
                 print("Your input was not a valid integer")
 
