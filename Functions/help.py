47bff9dd4fe7f58e712a08211095259a6c28fdfb
MaxO <maxi.opel.16@gmail.com>
2025-05-07 20:40:11 +0200

    adding the help page for the binary system

--- Functions/help.py
+++ Functions/help.py
@@ -6,21 +6,30 @@ def help_and_descriptions():
           "In any other case its only the command itself\n"
           "\n"
           "\n"
+          
           "BASIC COMMANDS \n"
           "- do {your calculation}                  input a calculation and the shell solves it\n"
           "- exit                                   exits the shell\n"
           "- help                                   opens this help menu\n"
           "\n"
           "\n"
+          
           "PRIME NUMBERS \n"
           "- is prime {a number}                    checks if your number is a prime number\n"
           "- prime range                            gives back a list of all primes in your range\n"
           "\n"
           "\n"
+          
+          "NUMBER SYSTEMS \n"
+          "- bin {a number}                         gives back your number calculated in the binary system\n"
+          "\n"
+          "\n"
+          
           "STATISTICS \n"
-          "- binom dis                              gives you all specific infos about your binominal distribution\n"
+          "- binom dis                              ask and calculates all specific infos about your binominal distribution\n"
           "\n"
           "\n"
+          
           "SEQUENCE / SERIE \n"
           "- fib {n}                                gives back a list of n elements of the fibonacci sequence\n"
           "\n"
