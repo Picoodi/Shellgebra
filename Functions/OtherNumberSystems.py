01701978879983e9d64693d8899118189a36035f
MaxO <maxi.opel.16@gmail.com>
2025-05-07 20:22:25 +0200

    adding a new file

---
+++ Functions/OtherNumberSystems.py
@@ -0,0 +1,10 @@
+def binary_system(number):
+    binary_number = ""
+    while number != 0:
+        if number %2 = True:
+            binary_number = binary_number +"0"
+        else:
+            binary_number = binary_number +"1"
+    else:
+        binary_number ="0"
+    return binary_number
